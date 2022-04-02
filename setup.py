4119e1166611c095e518048814cc7a428243e731
sz <sz@recv.cc>
2022-04-01 21:08:15 -0500

    Give us a patch digit

--- setup.py
+++ setup.py
@@ -71,7 +71,7 @@ class CMakeBuild(build_ext):
 
 
 def read_version():
-    return '0.1'  # pull version from libmcleece, eventually
+    return '0.1.0'  # pull version from libmcleece, eventually
 
 
 setup(
